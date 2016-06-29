12b0f27556f301aef2cf6747fa9d1c5430c03012
Sumit Mukhija <sumit.mukhija@care.com>
2016-06-29 16:53:40 +0530

    Image files added for other navigation bar buttons

--- iMail_CloudmagicTask/InboxListViewController.swift
+++ iMail_CloudmagicTask/InboxListViewController.swift
@@ -1,7 +1,7 @@
 //
 //  ViewController.swift
 //  iMail_CloudmagicTask
-//
+// 
 //  Created by Sumit Mukhija on 29/06/16.
 //  Copyright © 2016 Sumit Mukhija. All rights reserved.
 //
@@ -9,17 +9,51 @@
 import UIKit
 
 class InboxListViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setVisuals()
     }
+    
+    func setVisuals(){
+        setViewVisualAttributes()
+        setNavigationVisualttributes()
+    }
+    
+    func setViewVisualAttributes(){
+        self.title = "Inbox"
+        self.view.backgroundColor = AppColorTheme.primaryBackgroundColor
+    }
+    
+    func setNavigationVisualttributes(){
+        self.navigationController?.navigationBar.barTintColor = AppColorTheme.themePrimaryColor;
+       self.navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
+    }
+    
+//    -(void)setNavigationVisualProperties{
+//    self.navigationController.navigationBar.barTintColor = [UIColor orangeColor];
+//    [self.navigationController.navigationBar setTintColor:[UIColor whiteColor]];
+//    UIImage *image = [UIImage imageNamed:@"reload"];
+//    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+//    button.bounds = CGRectMake( 0, 0, image.size.width, image.size.height );
+//    [button setImage:image forState:UIControlStateNormal];
+//    [button addTarget:self action:@selector(refreshPressed:) forControlEvents:UIControlEventTouchUpInside];
+//    UIBarButtonItem *refreshBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:button];
+//    UIImage *infoImage = [UIImage imageNamed:@"info"];
+//    UIButton *infoButton = [UIButton buttonWithType:UIButtonTypeCustom];
+//    infoButton.bounds = CGRectMake( 0, 0, image.size.width, image.size.height );
+//    [infoButton setImage:infoImage forState:UIControlStateNormal];
+//    [infoButton addTarget:self action:@selector(infoPressed:) forControlEvents:UIControlEventTouchUpInside];
+//    UIBarButtonItem *infoBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:infoButton];
+//    
+//    self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName : [UIColor whiteColor]};
+//    self.title = @"Top News";
+//    self.navigationItem.rightBarButtonItem = refreshBarButtonItem;
+//    self.navigationItem.leftBarButtonItem = infoBarButtonItem;
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
