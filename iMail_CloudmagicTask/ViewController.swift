e90a00f9054db30803d5f7eaf47df5e78d966e72
Sumit Mukhija <sumit.mukhija@care.com>
2016-06-29 16:08:44 +0530

    Initial commit!

---
+++ iMail_CloudmagicTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iMail_CloudmagicTask
+//
+//  Created by Sumit Mukhija on 29/06/16.
+//  Copyright © 2016 Sumit Mukhija. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
