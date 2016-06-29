538870d0c1aaef2521a307e1e1329276c815f328
Sumit Mukhija <sumit.mukhija@care.com>
2016-06-29 16:30:07 +0530

    Added AppColorTheme, MailContentViewController

---
+++ iMail_CloudmagicTask/MailContentViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MailContentViewController.swift
+//  iMail_CloudmagicTask
+//
+//  Created by Sumit Mukhija on 29/06/16.
+//  Copyright © 2016 Sumit Mukhija. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MailContentViewController:UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
