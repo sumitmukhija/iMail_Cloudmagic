4a3187eb5b32b80fafd2d2136d79a0d82f01a907
Sumit Mukhija <sumit.mukhija@care.com>
2016-06-30 14:26:30 +0530

    Loading screen added

---
+++ iMail_CloudmagicTask/Views/LoadingScreen.swift
@@ -0,0 +1,13 @@
+//
+//  LoadingScreen.swift
+//  iMail
+//
+//  Created by Sumit Mukhija on 30/06/16.
+//  Copyright © 2016 Sumit Mukhija. All rights reserved.
+//
+
+import UIKit
+
+class LoadingScreen: UIView {
+    
+}
