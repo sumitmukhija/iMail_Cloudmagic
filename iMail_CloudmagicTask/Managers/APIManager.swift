dd79d8166e9e59919eefee3c08ce0da8155e385f
Sumit Mukhija <sumit.mukhija@care.com>
2016-06-30 14:03:13 +0530

    image xcasset changes

---
+++ iMail_CloudmagicTask/Managers/APIManager.swift
@@ -0,0 +1,14 @@
+//
+//  APIManager.swift
+//  iMail
+//
+//  Created by Sumit Mukhija on 30/06/16.
+//  Copyright © 2016 Sumit Mukhija. All rights reserved.
+//
+
+import Foundation
+class APIManager{
+    
+    
+    
+}
