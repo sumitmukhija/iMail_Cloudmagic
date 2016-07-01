4fc774637b0e7d325775a7092ebccedffa4b069b
Sumit Mukhija <sumit.mukhija@care.com>
2016-07-01 13:09:04 +0530

    Email model object added

--- iMail_CloudmagicTask/Models/Email.swift
+++ iMail_CloudmagicTask/Models/Email.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+class Email: NSObject{
+    var mailId:Int
+    var isMailRead:Bool, isMailStarred:Bool
+    var peopleInvolved:[String]
+    var mailPreview:String
+    var mailSubject:String
+    var body:String?
+    
+    init(dataDictionary : [String : AnyObject]) {
+        mailId = dataDictionary["id"] as! Int
+        isMailRead = dataDictionary["isRead"] as! Bool
+        isMailStarred = dataDictionary["isStarred"] as! Bool
+        peopleInvolved = dataDictionary["participants"] as! [String]
+        mailPreview = dataDictionary["preview"] as! String
+        mailSubject = dataDictionary["subject"] as! String
+    }
+}
