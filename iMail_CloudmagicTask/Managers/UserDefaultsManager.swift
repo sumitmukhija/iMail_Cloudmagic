edc283460b4fd08f9a28ee94a90424c1337598c6
Sumit Mukhija <sumit.mukhija@care.com>
2016-07-01 17:36:33 +0530

    UserdefaultsManager added

---
+++ iMail_CloudmagicTask/Managers/UserDefaultsManager.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  iMail
+//
+//  Created by Sumit Mukhija on 01/07/16.
+//  Copyright © 2016 Sumit Mukhija. All rights reserved.
+//
+
+import Foundation
+
+class UserDefaultsManager: NSObject{
+    
+    func storeAMail(mail: Email, key: String){
+        
+    }
+    
+    func getAMail() -> Email{
+        return Email()
+    }
+    
+    func removeAMail(mailId:Int){
+        
+    }
+    
+    
+}
